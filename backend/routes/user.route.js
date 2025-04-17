f8257a7fae3b39c4b0d7d135787165ff53e150b0
Esther304 <1155194427@link.cuhk.edu.hk>
2025-04-18 02:17:23 +0800

    deletion on backend

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,7 +1,3 @@
-import express from 'express';
-import bcryp from 'bcrypt';
-import {User} from '../modeks/User.model.js';
-
 const express = require("express")
 const router = express.Router();
 const { getUsers, getUser, createUser, updateUser, deleteUser } = require('../controllers/user.controller')
@@ -20,22 +16,5 @@ router.put("/:id", updateUser);
 
 // delete a user
 router.delete("/:id", deleteUser);
-
-router.post('/signup', (req, res) => {
-    const {username, email, password} = req.body;
-    const User.find({email})
-    if(user) {
-        return res.json({message: "User already existed"})
-    }
-    const hashpassword = await bcryt.hash(password, 10)
-    const newUser = new User({
-        username,
-        email,
-        passsword: hashpassword,
-    })
-
-    await newUser.save()
-    return res.json({message: "record registered"})
-})
 
 module.exports = router;
